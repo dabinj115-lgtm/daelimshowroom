7047bbbfc92e76ad0d253685165441ea580572ba
dabinj115-lgtm <dabinj115@gmail.com>
2026-07-09 21:07:41 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-document.querySelectorAll('.map-button').forEach((button) => {
-  button.addEventListener('touchstart', () => button.classList.add('is-touched'), { passive: true });
-  button.addEventListener('touchend', () => window.setTimeout(() => button.classList.remove('is-touched'), 220), { passive: true });
-});
+(function(){
+  const buttons = document.querySelectorAll('.map-button');
+  buttons.forEach((button)=>{
+    button.addEventListener('click',(event)=>{
+      const url = button.href;
+      const target = button.target || '_self';
+      event.preventDefault();
+      button.classList.add('is-touched');
+      window.setTimeout(()=>{
+        if(target === '_blank') window.open(url, '_blank', 'noopener,noreferrer');
+        else window.location.href = url;
+        button.classList.remove('is-touched');
+      }, 120);
+    });
+  });
+})();
